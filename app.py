0e887c3c5e50367d0583eaa6882ec3f740f1c30f
Felth888 <70535935+Felth888@users.noreply.github.com>
2021-04-30 18:13:38 -0400

    =UPDATE(app.py app.run)
    added host and port values

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def new_game():
     return render_template('new-game.html', form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 
